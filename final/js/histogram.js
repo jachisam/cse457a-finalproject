97033f3a72b7ee8aefe3bb295266463382dd576a
wustlcse <43586038+wustlcse@users.noreply.github.com>
2019-04-28 21:05:15 -0500

    Update histogram.js

--- final/js/histogram.js
+++ final/js/histogram.js
@@ -1,7 +1,8 @@
-var margin = {top: 10, right: 30, bottom: 50, left: 40},
+var margin = {top: 10, right: 30, bottom: 50, left: 50},
     width = 500 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+// append the svg and g for the histogram
 var svg = d3.select("#multiple_histogram")
     .append("svg")
     .attr("id","multiple_histogram_svg")
@@ -12,6 +13,7 @@ var svg = d3.select("#multiple_histogram")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
+// y axis label
 svg.append("text")
     .attr("id","y_axis_label")
     .attr("transform", "rotate(-90)")
@@ -21,14 +23,14 @@ svg.append("text")
     .style("text-anchor", "middle")
     .text("Count");
 
+// x axis label default to GRE_Score
 svg.append("text")
     .attr("id","x_axis_label")
-    .attr("transform",
-        "translate(" + (width/2) + "," +
+    .attr("transform", "translate(" + (width/2) + "," +
         (height + 30) + ")")
     .style("text-anchor", "middle")
         .text("GRE_Score");
-
+// selection box with options = factors that affect admission, e.g., GPA, GRE_Score etc
 var select = d3.select('#select_div')
     .append('select')
     .attr("id","my_select")
@@ -36,6 +38,7 @@ var select = d3.select('#select_div')
     .on('change',selectionchange)
 ;
 
+// dict such that each admission factor gets its corresponding color
 var color_for_each_factor_dict = {};
 
 
@@ -47,15 +50,24 @@ var y = d3.scaleLinear();
 x_axis = svg.append("g").attr("id","x_axis");
 y_axis = svg.append("g").attr("id","y_axis");
 
+// global variable to hold the data that is read
 var data_as_global_variable;
+
+// store te current selection box selection; default to GRE_Score
 var factor_selected = "GRE_Score";
 
 initialize_multiple_histogram();
 
+/*
+Initialize a histogram that can be used to
+visualize the distributions of different admission factors
+ */
 function initialize_multiple_histogram() {
+
 d3.csv(my_graduate_admissions_data
     , function(data) {
 
+        // get the names of the admission factors
         var factors = data.columns;
         for(var i = 0; i < factors.length; ++i)
         {
@@ -63,8 +75,7 @@ d3.csv(my_graduate_admissions_data
             =randomcolor();
         }
         data_as_global_variable = data;
-        //delete factors[factors.indexOf('Serial_No')];
-        //factors.unshift("     ");
+        // populate the options
         var options = select
             .selectAll('option')
             .data(factors.slice(1,factors.length)).enter()
@@ -77,9 +88,11 @@ d3.csv(my_graduate_admissions_data
             })
             .text(function (d) { return d; });
 
+        // set the default color to be the color of GRE_Score
         $('#my_select')
             .css("color",color_for_each_factor_dict['GRE_Score']);
 
+        // x axis
         x.domain([d3.min(data, function(d) { return +d[factor_selected]; }),
             d3.max(data, function(d) { return +d[factor_selected]; })])
             .range([0, width]);
@@ -87,18 +100,22 @@ d3.csv(my_graduate_admissions_data
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
+
         var histogram = d3.histogram()
             .value(function(d) { return d[factor_selected]; })
             .domain(x.domain())
             .thresholds(20);
 
+        // set up the bins
         var bins = histogram(data);
 
+        // y axis
         y.range([height, 0]);
         y.domain([0, d3.max(bins, function(d) { return d.length; })]);
         d3.select("#y_axis")
             .call(d3.axisLeft(y));
 
+        // generate the rects
         svg.selectAll("rect")
             .data(bins)
             .enter()
@@ -115,10 +132,12 @@ d3.csv(my_graduate_admissions_data
 function selectionchange() {
     var selectValue = d3.select('select').property('value');
 
+    // update the color of the menu
     $('#my_select').css("color", $("select option:selected").css("color"));
 
     console.log(selectValue);
 
+    // update the histogram from here on
     factor_selected = selectValue;
     x.domain([d3.min(data_as_global_variable, function(d) { return +d[factor_selected]; }),
         d3.max(data_as_global_variable, function(d) { return +d[factor_selected]; })])
@@ -142,9 +161,11 @@ function selectionchange() {
         .call(d3.axisLeft(y));
 
 
+    // remove older rects
 
     svg.selectAll("rect").remove();
 
+    // update the histogram and fill in new color
     svg.selectAll("rect")
         .data(bins)
         .enter()
@@ -158,6 +179,11 @@ function selectionchange() {
     d3.select("#x_axis_label").text(factor_selected);
 }
 
+/*
+Generate a string that represents a random color
+Make the B part larger than R part for a #RRGGBB such
+that more likely to get a cool color
+ */
 function randomcolor() {
     var l = '0123456789ABCDEF';
     var c = '#';
