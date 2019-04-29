69759eca9138b5534a9b9254b20555a04dd62c46
professorengima <sharris22@wustl.edu>
2019-04-28 19:26:03 -0500

    Add files via upload
    Final Updates.

--- final/js/bubble_chart.js
+++ final/js/bubble_chart.js
@@ -1,15 +1,13 @@
-//grant_title,id,organization,Chance_of_Admit,group,Grant start date,start_month,start_day,start_year
-//GRE_Score,TOEFL_Score,University_Rating,SOP,LOR,CGPA,Research,Chance_of_Admit
-
-/* bubbleChart creation function. Returns a function that will
-* instantiate a new bubble chart given a DOM element to display
-* it in and a dataset to visualize.
-*
-* University_Rating and style inspired by:
-* https://bost.ocks.org/mike/chart/
-
-*The Idea and skeleton was taken from the bill gates grant bubble chart:*
+/*
+*The Idea and skeleton was taken from the bill gates grant bubble chart:
 http://vallandingham.me/gates_bubbles/
+
+This implementation was initially made for donation funds and three
+distinct years, however, I really liked the idea of using bubbles for
+selecting items so it has been drastically modified to fit that purpose.
+We kept a subset of the functions and modified all of the remain functions
+to fit our dataset.
+
 */
 
 
@@ -18,22 +16,15 @@ var pureData = [];
 function bubbleChart()
 {
   // Constants for sizing
-  //var width = 940;
-  //var height = 800;
-
-  // var width = 663;
-  // var height = 400;
-
   var width = 450;
   var height = 450;
 
-
-  // tooltip for mouseover functionality
-  //  var tooltip = floatingTooltip('gates_tooltip', 240);
-
   // Locations to move bubbles towards, depending
   // on which view mode is selected.
-  var center = { x: width / 2, y: height / 2 };
+  var center = {
+    x: width / 2,
+    y: height / 2
+  };
 
   // @v4 strength to apply to the position forces
   var forceStrength = 0.3;
@@ -47,17 +38,6 @@ function bubbleChart()
   // Charge function that is called for each node.
   // As part of the ManyBody force.
   // This is what creates the repulsion between nodes.
-  //
-  // Charge is proportional to the diameter of the
-  // circle (which is stored in the radius attribute
-  // of the circle's associated data.
-  //
-  // This is done to allow for accurate collision
-  // detection with nodes of different sizes.
-  //
-  // Charge is negative because we want nodes to repel.
-  // @v4 Before the charge was a stand-alone attribute
-  //  of the force layout. Now we can use it as a separate force!
   function charge(d)
   {
     return -Math.pow(d.radius, 2.0) * forceStrength;
@@ -67,11 +47,11 @@ function bubbleChart()
   // @v4 We create a force simulation now and
   //  add forces to it.
   var simulation = d3.forceSimulation()
-  .velocityDecay(0.2)
-  .force('x', d3.forceX().strength(forceStrength).x(center.x))
-  .force('y', d3.forceY().strength(forceStrength).y(center.y))
-  .force('charge', d3.forceManyBody().strength(charge))
-  .on('tick', ticked);
+    .velocityDecay(0.2)
+    .force('x', d3.forceX().strength(forceStrength).x(center.x))
+    .force('y', d3.forceY().strength(forceStrength).y(center.y))
+    .force('charge', d3.forceManyBody().strength(charge))
+    .on('tick', ticked);
 
   // @v4 Force starts up automatically,
   //  which we don't want as there aren't any nodes yet.
@@ -80,36 +60,25 @@ function bubbleChart()
 
   var fillColor = d3.scaleOrdinal(d3.schemeCategory10);
 
-  function createNodes(rawData) {
-    // Use the max Chance_of_Admit in the data as the max in the scale's domain
-    // note we have to ensure the Chance_of_Admit is a number.
-    //  var maxAmount = d3.max(rawData, function (d) { return +d.Chance_of_Admit; });
-
-    // Sizes bubbles based on area.
-    // @v4: new flattened scale names.
-    /*  var radiusScale = d3.scalePow()
-    .exponent(0.5)
-    .range([0, 1])
-    .domain([0, 0.9]);
-    */
-
+  function createNodes(rawData)
+  {
     // Use map() to convert raw data into node data.
     // Checkout http://learnjsdata.com/ for more on
     // working with data.
-    var myNodes = rawData.map(function (d,index)
+    var myNodes = rawData.map(function(d, index)
     {
 
       var clr = "";
       var val = +d.Chance_of_Admit;
       var result = [];
 
-      if(val > 0.89)
+      if (val > 0.89)
       {
         clr = "#C3D7EE";
       }
       else
       {
-        if( (0.7 < val) && (val <= 0.89))
+        if ((0.7 < val) && (val <= 0.89))
         {
           clr = "#2774AE";
         }
@@ -119,169 +88,179 @@ function bubbleChart()
         }
       }
 
-      result.id= "c"+index;
-      result.color= clr;
-      //radius= radiusScale(+d.Chance_of_Admit),
-      result.radius= 9;//*(+d.Chance_of_Admit);
-      result.value= +d.Chance_of_Admit;
-      result.name= index;
-      result.org= d.University_Rating;
-      result.group= d.CGPA;
+      result.id = "c" + index;
+      result.color = clr;
+      result.radius = 9;
+      result.value = +d.Chance_of_Admit;
+      result.name = index;
+      result.org = d.University_Rating;
+      result.group = d.CGPA;
       result.CGPA = d.CGPA;
-      result.gre= d.GRE_Score;
-      result.x= Math.random() * 900;
-      result.y= Math.random() * 800;
+      result.gre = d.GRE_Score;
+      result.x = Math.random() * 900;
+      result.y = Math.random() * 800;
       return result;
     });
 
     // sort them to prevent occlusion of smaller nodes.
-    myNodes.sort(function (a, b) { return b.value - a.value; });
+    myNodes.sort(function(a, b)
+    {
+      return b.value - a.value;
+    });
 
     return myNodes;
   }
 
   /*
-  * Main entry point to the bubble chart. This function is returned
-  * by the parent closure. It prepares the rawData for visualization
-  * and adds an svg element to the provided selector and starts the
-  * visualization creation process.
-  *
-  * selector is expected to be a DOM element or CSS selector that
-  * points to the parent element of the bubble chart. Inside this
-  * element, the code will add the SVG continer for the visualization.
-  *
-  * rawData is expected to be an array of data objects as provided by
-  * a d3 loading function like d3.csv.
-  */
-  var chart = function chart(selector, rawData) {
+   * Main entry point to the bubble chart. This function is returned
+   * by the parent closure. It prepares the rawData for visualization
+   * and adds an svg element to the provided selector and starts the
+   * visualization creation process.
+   *
+   * selector is expected to be a DOM element or CSS selector that
+   * points to the parent element of the bubble chart. Inside this
+   * element, the code will add the SVG continer for the visualization.
+   *
+   * rawData is expected to be an array of data objects as provided by
+   * a d3 loading function like d3.csv.
+   */
+  var chart = function chart(selector, rawData)
+  {
     // convert raw data into nodes data
     nodes = createNodes(rawData);
 
     // Create a SVG element inside the provided selector
     // with desired size.
     svg = d3.select(selector)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
 
     // Bind nodes data to what will become DOM elements to represent them.
     bubbles = svg.selectAll('.bubble')
-    .data(nodes, function (d) { return d.id; });
+      .data(nodes, function(d)
+      {
+        return d.id;
+      });
 
 
     texts = svg.selectAll(null)
-    .data(rawData)
-    .enter()
-    .append('text')
-    .text(d => d.Chance_of_Admit)
-    .attr('color', 'black')
-    .attr('font-size', 1);
+      .data(rawData)
+      .enter()
+      .append('text')
+      .text(d => d.Chance_of_Admit)
+      .attr('color', 'black')
+      .attr('font-size', 1);
 
     // add the tooltip area to the webpage
     var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
 
     // Create new circle elements each with class `bubble`.
     // There will be one circle.bubble for each object in the nodes array.
-    // Initially, their radius (r attribute) will be 0.
-    // @v4 Selections are immutable, so lets capture the
-    //  enter selection to apply our transtition to below.
     var bubblesE = bubbles.enter().append('circle')
-    .classed('bubble', true)
-    .attr('r', 50)
-    .attr('fill', function (d){
-      if(d.value > 0.89)
+      .classed('bubble', true)
+      .attr('r', 50)
+      .attr('fill', function(d)
       {
-        return "#C3D7EE";
-      }
-      else
-      {
-        if( (0.7 < d.value) && (d.value <= 0.89))
+        if (d.value > 0.89)
         {
-          return "#2774AE";
+          return "#C3D7EE";
         }
         else
         {
-          return "#003B5C";
+          if ((0.7 < d.value) && (d.value <= 0.89))
+          {
+            return "#2774AE";
+          }
+          else
+          {
+            return "#003B5C";
+          }
         }
-      }
-    })
-    .attr("id",function(d){return d.id;})
-    .attr('stroke', function (d) { return "black";})
-    .attr('stroke-width', 2)
-    .on("mouseover", function(d){
-      tooltip.transition() // http://bl.ocks.org/weiglemc/6185069
-      .duration(200)
-      .style("opacity", 1);
-      tooltip.html(
-        '<span class="name">GPA: </span><span class="value">' + d.CGPA +
-        '</span><br/>' +
-        '<span class="name">GRE Score: </span><span class="value">' + d.gre +
-        '</span><br/>' +
-        '<span class="name">Chance of Admission: </span><span class="value">' +
-        Math.round(addCommas(d.value*100)) + "%"
-       )
-      .style("left", (d3.event.pageX + 4) + "px")
-      .style("top", (d3.event.pageY - 24) + "px");
-
-      d3.select(this).style("fill","#ffc107");
-      d3.select(this).attr("r",d.radius*1.5);
-
-      svg.selectAll('.bubble')
-      .data(nodes)
-      .transition()
-      .duration(1200)
-      .style("opacity", function(b)
+      })
+      .attr("id", function(d)
       {
-        if(b.id != d.id)
-        {
-          d3.select("#"+b.id).style("fill",b.color).attr("r",b.radius);
-
-        }
-        if( (b.id != d.id) && (b.color != d.color))
-        {
-          return 0.1;
-        }
-        else
-        {
-          return 1;
-        }
-      });
-
-    })
-    .on("mouseout", function(d)
-    {
-      tooltip.transition()
-      .duration(500)
-      .style("opacity", 0);
-
-
-      svg.selectAll('.bubble') .data(nodes)
-      .transition() .duration(1000)
-
-      .style("opacity", function(b)
+        return d.id;
+      })
+      .attr('stroke', function(d)
       {
-        d3.select("#"+b.id).style("fill",b.color).attr("r",b.radius);
-        return 1;
+        return "black";
+      })
+      .attr('stroke-width', 2)
+      .on("mouseover", function(d)
+      {
+        tooltip.transition() // http://bl.ocks.org/weiglemc/6185069
+          .duration(200)
+          .style("opacity", 1);
+        tooltip.html(
+            '<span class="name">GPA: </span><span class="value">' + d.CGPA +
+            '</span><br/>' +
+            '<span class="name">GRE Score: </span><span class="value">' + d.gre +
+            '</span><br/>' +
+            '<span class="name">Chance of Admission: </span><span class="value">' +
+            Math.round(addCommas(d.value * 100)) + "%"
+          )
+          .style("left", (d3.event.pageX + 4) + "px")
+          .style("top", (d3.event.pageY - 24) + "px");
+
+        d3.select(this).style("fill", "#ffc107");
+        d3.select(this).attr("r", d.radius * 1.5);
+
+        svg.selectAll('.bubble')
+          .data(nodes)
+          .transition()
+          .duration(1200)
+          .style("opacity", function(b)
+          {
+            if (b.id != d.id)
+            {
+              d3.select("#" + b.id).style("fill", b.color).attr("r", b.radius);
+
+            }
+            if ((b.id != d.id) && (b.color != d.color))
+            {
+              return 0.1;
+            }
+            else
+            {
+              return 1;
+            }
+          });
 
       })
-    })
+      .on("mouseout", function(d)
+      {
+        tooltip.transition()
+          .duration(500)
+          .style("opacity", 0);
 
-    .on("click", function(d,i)
-    {
-      clearApplicantTable();
-      pureData[parseInt(d.id.substring(1,d.id.length))].Serial_No = d.name;
-      drawApplicantTable(pureData[parseInt(d.id.substring(1,d.id.length))]);
+        svg.selectAll('.bubble').data(nodes)
+          .transition().duration(1000)
 
-      d3.select(this).style("fill","#ffc107");
-      d3.select(this).attr("r",d.radius*1.5);
+          .style("opacity", function(b)
+          {
+            d3.select("#" + b.id).style("fill", b.color).attr("r", b.radius);
+            return 1;
 
-      generate_chart(d);
+          })
+      })
 
-      window.location = '#applicant-table'
-    });
+      .on("click", function(d, i)
+      {
+        clearApplicantTable();
+        pureData[parseInt(d.id.substring(1, d.id.length))].Serial_No = d.name;
+        drawApplicantTable(pureData[parseInt(d.id.substring(1, d.id.length))]);
+
+        d3.select(this).style("fill", "#ffc107");
+        d3.select(this).attr("r", d.radius * 1.5);
+
+        generate_chart(d);
+
+        window.location = '#applicant-table'
+      });
 
     // @v4 Merge the original empty selection and the enter selection
     bubbles = bubbles.merge(bubblesE);
@@ -289,8 +268,11 @@ function bubbleChart()
     // Fancy transition to make bubbles appear, ending with the
     // correct radius
     bubbles.transition()
-    .duration(2000)
-    .attr('r', function (d) { return d.radius; });
+      .duration(2000)
+      .attr('r', function(d)
+      {
+        return d.radius;
+      });
 
     // Set the simulation's nodes to our newly created nodes array.
     // @v4 Once we set the nodes, the simulation will start running automatically!
@@ -301,32 +283,33 @@ function bubbleChart()
   };
 
   /*
-  * Callback function that is called after every tick of the
-  * force simulation.
-  * Here we do the acutal repositioning of the SVG circles
-  * based on the current x and y values of their bound node data.
-  * These x and y values are modified by the force simulation.
-  */
-  function ticked() {
+   * Callback function that is called after every tick of the
+   * force simulation.
+   */
+  function ticked()
+  {
     bubbles
-    .attr('cx', function (d) { return d.x; })
-    .attr('cy', function (d) { return d.y; });
+      .attr('cx', function(d)
+      {
+        return d.x;
+      })
+      .attr('cy', function(d)
+      {
+        return d.y;
+      });
 
-    texts.attr('x', (data) => {
-      return data.x
-    })
-    .attr('y', (data) => {
-      return data.y
-    });
+    texts.attr('x', (data) =>
+      {
+        return data.x
+      })
+      .attr('y', (data) =>
+      {
+        return data.y
+      });
   }
 
-  /*
-  * Sets visualization in "single group mode".
-  * The year labels are hidden and the force layout
-  * tick function is set to move all nodes to the
-  * center of the visualization.
-  */
-  function groupBubbles(){
+  function groupBubbles()
+  {
     // @v4 Reset the 'x' force to draw the bubbles to the center.
     simulation.force('x', d3.forceX().strength(forceStrength).x(center.x));
 
@@ -334,107 +317,26 @@ function bubbleChart()
     simulation.alpha(1).restart();
   }
 
-
-  /*
-  * Sets visualization in "split by year mode".
-  * The year labels are shown and the force layout
-  * tick function is set to move nodes to the
-  * yearCenter of their data's year.
-  */
-  function splitBubbles(){
-
-    // @v4 Reset the 'x' force to draw the bubbles to their year centers
-    simulation.force('x', d3.forceX().strength(forceStrength).x(nodeYearPos));
-
-    // @v4 We can reset the alpha value and restart the simulation
-    simulation.alpha(1).restart();
-  }
-
-  /*
-  * Function called on mouseover to display the
-  * details of a bubble in the tooltip.
-  */
-/*  function showDetail(d)
-  {
-    // change outline to indicate hover state.
-    d3.select(this)
-    .attr('stroke', 'red')
-    .attr('stroke-width',5);
-
-    var content = '<span class="name">Applicant Number: </span><span class="value">' +
-    d.name +
-    '</span><br/>' +
-    '<span class="name">Chance of Admission: </span><span class="value">' +
-    addCommas(d.value*100) + "%" +
-    '</span><br/>' +
-    '<span class="name">GRE Score: </span><span class="value">' +
-    d.gre + '<br/><span class="name">More stats to come </span><span class="value">' +
-
-    '</span>';
-
-    tooltip.showTooltip(content, d3.event);
-    //console.dir(bubbles._groups[0][d.id].attributes);
-  }*/
-
-  /*
-  * Hides tooltip
-  */
-/*
-  function hideDetail(d)
-  {
-    // reset outline
-    d3.select(this)
-    .attr('stroke', "black")
-    .attr('stroke-width',1);
-
-    tooltip.hideTooltip();
-    var bubbleMod = svg.selectAll('.bubble')
-    .data(nodes)
-    .transition()
-    .duration(500)
-    .style("opacity", function(b) {return 1;});
-  }
-*/
-
-
-  /*
-  * Externally accessible function (this is attached to the
-  * returned chart function). Allows the visualization to toggle
-  * between "single group" and "split by year" modes.
-  *
-  * displayName is expected to be a string and either 'year' or 'all'.
-  */
-  /*chart.toggleDisplay = function (displayName)
-  {
-    if (displayName === 'year')
-    {
-      splitBubbles();
-    } else {
-      groupBubbles();
-    }
-  };
-*/
-
   // return the chart function from closure.
   return chart;
 }
 
 /*
-* Below is the initialization code as well as some helper functions
-* to create a new bubble chart instance, load the data, and display it.
-*/
+ * Below is the initialization code as well as some helper functions
+ * to create a new bubble chart instance, load the data, and display it.
+ */
 
 var myBubbleChart = bubbleChart();
 
 /*
-* Function called once data is loaded from CSV.
-* Calls bubble chart function to display inside #vis div.
-*/
+ * Function called once data is loaded from CSV.
+ * Calls bubble chart function to display inside #vis div.
+ */
 function display(error, data)
 {
   if (error)
   {
-    //console.log(error);
+    console.log(error);
   }
 
   pureData = data;
@@ -444,9 +346,9 @@ function display(error, data)
 
 
 /*
-* Helper function to convert a number into a string
-* and add commas to it to improve presentation.
-*/
+ * Helper function to convert a number into a string
+ * and add commas to it to improve presentation.
+ */
 function addCommas(nStr)
 {
   nStr += '';
@@ -454,7 +356,8 @@ function addCommas(nStr)
   var x1 = x[0];
   var x2 = x.length > 1 ? '.' + x[1] : '';
   var rgx = /(\d+)(\d{3})/;
-  while (rgx.test(x1)) {
+  while (rgx.test(x1))
+  {
     x1 = x1.replace(rgx, '$1' + ',' + '$2');
   }
 
@@ -462,4 +365,4 @@ function addCommas(nStr)
 }
 
 // Load the data.
-d3.csv('../data/graduate-admissions/admission_cleaned.csv',display);
+d3.csv('../data/graduate-admissions/admission_cleaned.csv', display);
--- final/js/student.bar.chart.js
+++ final/js/student.bar.chart.js
@@ -1,5 +1,11 @@
-var studentData = [];
+/*
+  This bar chart was inspired by https://bl.ocks.org/seemantk/ec245e1f4e824e685982dd5d3fbb2fcc
+  We incorporated the svg creation methods into our code. We loved the format so
+  we adopted it for our work as it really brings out the data succinctly in our
+  visualization.
+*/
 
+var studentData = [];
 
 var width = 450;
 var height = 450;
@@ -23,108 +29,115 @@ function generate_chart(subject)
     studentData = data;
     convertToNumericData(studentData);
 
-    studentData.forEach(function(d,index)
+    studentData.forEach(function(d, index)
     {
-      //if(d.Chance_of_Admit >= threshold)
-      if( (d.Chance_of_Admit < threshold*1.05) || (d.Chance_of_Admit > threshold*0.95))
+      if ((d.Chance_of_Admit < threshold * 1.05) || (d.Chance_of_Admit > threshold * 0.95))
       {
         d.Serial_No = index;
         currentData.push(d);
       }
     });
 
-    display_chart(subject,currentData);
+    display_chart(subject, currentData);
   });
 
 }
 
-function display_chart(subject,data)
+function display_chart(subject, data)
 {
 
-d3.selectAll("#vis2 > *").remove();
+  d3.selectAll("#vis2 > *").remove();
 
-data.sort(function(a, b)
-{
-  return d3.ascending(a.Chance_of_Admit, b.Chance_of_Admit)
-});
+  data.sort(function(a, b)
+  {
+    return d3.ascending(a.Chance_of_Admit, b.Chance_of_Admit)
+  });
 
-  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  var margin = {
+      top: 20,
+      right: 20,
+      bottom: 30,
+      left: 40
+    },
     width = 600 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
 
   var x = d3.scaleBand()
     .rangeRound([0, width], .1)
-		.paddingInner(0.1);
+    .paddingInner(0.1);
 
-var y = d3.scaleLinear()
+  var y = d3.scaleLinear()
     .range([height, 0]);
 
-var xAxis = d3.axisBottom()
+  var xAxis = d3.axisBottom()
     .scale(x);
 
-var yAxis = d3.axisLeft()
+  var yAxis = d3.axisLeft()
     .scale(y)
     .ticks(11, "%");
 
-var svg = d3.select("#vis2").append("svg")
+  var svg = d3.select("#vis2").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  x.domain(data.map(function(d) { return d.Serial_No; }));
-  //y.domain([0, d3.max(data, function(d) { return d.Chance_of_Admit; })]);
+  x.domain(data.map(function(d)
+  {
+    return d.Serial_No;
+  }));
   y.domain([0, 1]);
-  // svg.append("g")
-  //     .attr("class", "x axis")
-  //     .attr("transform", "translate(0," + height + ")")
-  //     .call(xAxis);
   svg.append("text")
-      // .attr("id","x_axis_label")
-      .attr("transform",
-          "translate(" + (width/2) + "," +
-          (height + 25) + ")")
-      .style("text-anchor", "middle")
-          .text("Applicant Pool");
+    .attr("transform",
+      "translate(" + (width / 2) + "," +
+      (height + 25) + ")")
+    .style("text-anchor", "middle")
+    .text("Applicant Pool");
 
   svg.append("text")
-      // .attr("id","y_axis_label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left-4)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Chance of Admittance");
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left - 4)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Chance of Admittance");
 
   svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
+    .attr("class", "y axis")
+    .call(yAxis)
     .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Frequency");
 
   svg.selectAll(".bar")
-      .data(data)
+    .data(data)
     .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d,i) { return x(d.Serial_No); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.Chance_of_Admit); })
-      .attr("fill",function(d)
+    .attr("class", "bar")
+    .attr("x", function(d, i)
+    {
+      return x(d.Serial_No);
+    })
+    .attr("width", x.bandwidth())
+    .attr("y", function(d)
+    {
+      return y(d.Chance_of_Admit);
+    })
+    .attr("fill", function(d)
+    {
+      if (d.Chance_of_Admit == subject.value)
       {
-        //if(d.Serial_No == subject.name)
-        if(d.Chance_of_Admit == subject.value)
-        {
-          return "#FFD100";
-        }
-        else
-        {
-            return "#2774AE";
-        }
-
-      })
-      .attr("height", function(d) { return height - y(d.Chance_of_Admit); });
+        return "#FFD100";
+      }
+      else
+      {
+        return "#2774AE";
+      }
+    })
+    .attr("height", function(d)
+    {
+      return height - y(d.Chance_of_Admit);
+    });
 }
